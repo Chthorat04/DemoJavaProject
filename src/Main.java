03834a8876ef0bea8c7b477c9de2310693f8e2d2
Chetana <thoratchetana4@gmail.com>
2025-01-13 08:39:36 -0500

    Updated output message in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git and GitHub!");
+        System.out.println("Hello, Git and GitHub with Changes!");
     }
 }
